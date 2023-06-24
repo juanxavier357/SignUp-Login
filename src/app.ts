441caaa3585ed917185ead105b0f646d769a2731
juanxavier357 <cabellosalirrosas@gmail.com>
2023-06-24 13:33:53 -0500

    fix: express server created

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, {Express, Request, Response, NextFunction} from 'express'
+
+const app: Express = express()
+
+const PORT = process.env.PORT ?? 8080
+
+// creando rutas
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    //res.send('Hello World')
+    res.json({message: 'Hello world'})
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running in http://localhost:${PORT}`)
+}) 
